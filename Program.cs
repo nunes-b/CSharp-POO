5a1f2d932a46211bb648c124b97a50ebf697f865
Rômulo Nunes <romulotg21@gmail.com>
2023-10-20 15:10:15 -0300

    feat: Progama

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ScreenSound;
+
+Musica musica1 = new Musica();
+musica1.nome = "Roxanne";
+musica1.artista = "The Police";
+musica1.duracao = 273;
+musica1.disponivel = true;
+
+musica1.ExibirFichaTecnica();
+
+Musica musica2 = new Musica();
+musica2.nome = "False BoDY";
+musica2.artista = "The Body";
+musica2.duracao = 123;
+musica2.disponivel = false;
+
+musica2.ExibirFichaTecnica();
